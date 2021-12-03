e10f04fc86231ca707b3b1e45256e764999bae0a
Tian Xu <tianxu2009@gmail.com>
2021-12-02 23:22:56 -0800

    2021 Day 1 Done

---
+++ 2021/day01/day01.jl
@@ -0,0 +1,38 @@
+function day1(fileName::String)
+	file = open(fileName)
+	input = readlines(file)
+	close(file)
+	input = parse.(Int64, input)
+
+	increasingCounter::Int64 = 0
+	for i in 2:length(input)
+		if input[i - 1] < input[i]
+			increasingCounter += 1
+		end
+	end
+
+	println(increasingCounter)
+end
+
+function day2(fileName::String)
+	file = open(fileName)
+	input = readlines(file)
+	close(file)
+	input = parse.(Int64, input)
+
+	increasingCounter::Int64 = 0
+	previous3::Int64 = input[1] + input[2] + input[3]
+	current3::Int64 = 0
+
+	for i in 2:length(input)-2
+		current3 = input[i] + input[i+1] + input[i+2]
+		if current3 > previous3
+			increasingCounter += 1
+		end
+		previous3 = current3
+	end
+
+	println(increasingCounter)
+end
+
+day2("input.txt")
